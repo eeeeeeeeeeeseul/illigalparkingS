d21727a05118a32d3106605f18ebf7ebb12f8654
eeeeeeeeeeeseul <es07011130@gmail.com>
2021-05-20 16:20:43 +0900

    serverfile upload

--- DLProject_serverTest/app/src/main/java/com/example/illegalparkingsearch/data/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.illegalparkingsearch.data;
-
-import java.io.Serializable;
-
-public class User implements Serializable {
-    public String username;
-    public String usernum;
-    public String location;
-}
